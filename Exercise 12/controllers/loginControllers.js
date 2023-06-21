7edbc139eb93cf03aa31b840c0be9de0eb39ba2f
ashna-24 <93767921+ashna-24@users.noreply.github.com>
2023-06-21 18:39:59 +0530

    commit

--- Exercise 12/controllers/loginControllers.js
+++ Exercise 12/controllers/loginControllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const token = require("../middlewares/tokens");
+const logger= require("../utils/logger");
 const jwt = require("jsonwebtoken");
 const jwtKey = "my_secret_key";
 
@@ -21,13 +22,15 @@ exports.user_create = asyncHandler(async(req, res, next) =>{
 
 	if (isPresent) {
 		const tokens = jwt.sign(token.userdata[isPresentIndex], jwtKey);
+  		logger.info(`Success login ${req.originalUrl} - ${req.method} - ${req.ip}`);
 		res.json({
-		  login: true,
-		  token: tokens,
-		  data: token.userdata[isPresentIndex],
+			login: true,
+			token: tokens,
+			data: token.userdata[isPresentIndex],
 		});
 	} 
 	else {
+  		logger.error(`${err.status || 500} - ${res.statusMessage} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
 		res.json({
 			login: false,
 			error: "please check name and password.",
@@ -42,12 +45,14 @@ exports.user_list =asyncHandler(async(req, res, next)=>{
 exports.user_welcome =asyncHandler(async(req,res,next)=>{
 	jwt.verify(req.token, jwtKey, (err, authdata)=>{
 		if(err){
+			logger.error(`${err.status || 500} - ${res.statusMessage} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
 			res.json({
 				message: false,
 				data: "error",
 			});
 		}
 		else{
+			logger.info(`Welcome user ${req.originalUrl} - ${req.method} - ${req.ip}`);
 			res.json({
 				message :"Welcome to profile",
 				data: authdata
--- Exercise 12/index.js
+++ Exercise 12/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const routes = require('./routes/sampleRoute');
 const login =require("./routes/loginRoute");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,5 +10,5 @@ app.use(routes);
 app.use(login);
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
